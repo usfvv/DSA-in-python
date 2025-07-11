283fc01b32d33db779e34cb1f45c35633650aef9
yousef <yousefalaa0@icloud.com>
2025-07-11 03:22:44 +0300

    update GraphList.py

--- DSA-in-python/GraphList.py
+++ DSA-in-python/GraphList.py
@@ -18,19 +18,13 @@ a = GraphList()
 
 a.add_edge("A", "B")
 a.add_edge("A", "C")
-a.add_edge("B", "A")
 a.add_edge("B", "D")
 a.add_edge("B", "E")
-a.add_edge("C", "A")
 a.add_edge("C", "D")
-a.add_edge("D", "C")
-a.add_edge("D", "B")
 a.add_edge("D", "E")
-a.add_edge("E", "B")
-a.add_edge("E", "D")
 
-a.display()     #A --> ['B', 'C', 'B', 'C']
-                #B --> ['A', 'A', 'D', 'E', 'D', 'E']
-                #C --> ['A', 'A', 'D', 'D']
-                #D --> ['B', 'C', 'C', 'B', 'E', 'E']
-                #E --> ['B', 'D', 'B', 'D']
+a.display()     #A --> ['B', 'C']
+                #B --> ['A', 'D', 'E']
+                #C --> ['A', 'D']
+                #D --> ['B', 'C', 'E']
+                #E --> ['B', 'D']
